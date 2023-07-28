e58a07bcaed504880feff6efc8dd831fa5ca5e20
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-07-28 08:58:44 +0545

    Implement router for fetching data from database

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
+
 import { connect } from 'mongoose';
 import connectDB from './config/db.js';
+import router from './routes/productRoutes.js';
+
 const port = process.env.PORT || 8001;
 
 connectDB(); //connecting to the database
@@ -19,16 +21,8 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-//route for serving all the products
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-//route for serving a specific products
-app.get('/api/products/:name', (req, res) => {
-  const product = products.find((p) => p.name === req.params.name);
-  res.json(product);
-});
+//using the productRoutes when the request has URL of api/products
+app.use('/api/products', router);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
